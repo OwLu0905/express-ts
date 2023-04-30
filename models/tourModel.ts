4bd37dd32306c2eb8d36958cf3674c994619e6eb
OwLu0905 <owlu0905@gmail.com>
2023-04-30 15:58:17 +0800

    add model

---
+++ models/tourModel.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+// NOTE Sehcma  && Schema Type
+import { tourSchema } from "../schema/tours";
+import type { TourType } from "../schema/tours";
+
+const toursSchema = new mongoose.Schema<TourType>(tourSchema);
+
+// TODO : create
+export const Tour = mongoose.model<TourType>("Tour", toursSchema);
