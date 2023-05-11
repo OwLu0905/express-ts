ddfe37d84c7ebdf66b2f96b3c9ee7b396a8265da
OwLu0905 <owlu0905@gmail.com>
2023-05-11 20:01:59 +0800

    add virtual schema

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -4,7 +4,25 @@ import mongoose from "mongoose";
 import { tourSchema } from "../schema/tours";
 import type { TourType } from "../schema/tours";
 
-const toursSchema = new mongoose.Schema<TourType>(tourSchema);
+const toursSchema = new mongoose.Schema<TourType>(tourSchema, {
+	toJSON: { virtuals: true },
+	toObject: { virtuals: true },
+});
+
+// toursSchema.virtual('durationWeeks').get(function(this:any) {
+//
+// 	return this.duration / 7
+// })
+// virtual property
+// console.log(this, "thioejfiowejfowe");
+interface IToursDocument extends mongoose.Document {
+	duration: number;
+}
+
+toursSchema.virtual("durationWeeks").get(function(this: IToursDocument) {
+	return this.get("duration");
+
+});
 
 // TODO : create
 export const Tour = mongoose.model("Tour", toursSchema);
